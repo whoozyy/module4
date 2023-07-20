cd72848cbbc94fec8bf2bdfdd02571a5f278f19c
Sisyann <ezdvs16@gmail.com>
2023-07-20 20:32:13 +0500

    'initial'

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index,top_sellers
+from .views import index,top_sellers,advertisement_post,advertisement,register,login,profile
 
 urlpatterns = [
     path('',index, name = "main-page"),
-    path('top-sellers',top_sellers,name = "top-sellers")
+    path('top-sellers',top_sellers,name = "top-sellers"),
+    path('ad_post',advertisement_post,name = "ad_post"),
+    path('adv',advertisement,name = "adv"),
+    path('register',register,name = "reg"),
+    path('login', login, name="log"),
+    path('profile', profile, name="prf"),
 ]
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -6,3 +6,17 @@ def index(request):
 
 def top_sellers(request):
     return render(request,"top-sellers.html")
+
+def advertisement_post(request):
+    return render(request,"advertisement-post.html")
+
+def advertisement(request):
+    return render(request,"advertisement.html")
+
+def register(request):
+    return render(request,"register.html")
+
+def login(request):
+    return render(request,"login.html")
+def profile(request):
+    return render(request,"profile.html")
