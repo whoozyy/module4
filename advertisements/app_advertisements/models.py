abd9da22dcf2e695e0f0f718f82495386eb45aa9
Sisyann <ezdvs16@gmail.com>
2023-08-01 18:40:24 +0500

    'initial'

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок",max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена",max_digits=10,decimal_places=2) #99999999,99
+    #999,99 - max_digits = 5,decimal_places = 2
+    auction = models.BooleanField("Торг",help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
