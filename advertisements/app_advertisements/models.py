6e039426c9ad33df94e3a581174d44d41871922a
Sisyann <ezdvs16@gmail.com>
2023-08-03 21:27:32 +0500

    'initial'

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -20,6 +20,7 @@ class Advertisement(models.Model):
     def created_date(self):
         from django.utils import timezone
         from django.utils.html import format_html
-
-
-
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green;font-weight: bold;">Сегодня в {}</span>',created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
