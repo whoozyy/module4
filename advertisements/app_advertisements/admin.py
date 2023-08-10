7ebd78418fca6b0338a195e6a34f497b46369232
Sisyann <ezdvs16@gmail.com>
2023-08-10 18:34:52 +0500

    'initial'

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ["id","title","description","price","created_date","auction"]
+    list_display = ["id","title","description","price","created_date","updated_at","auction"]
     list_filter = ["auction","created_at"]
     actions = ["make_auction_as_false","make_auction_as_true"]
     fieldsets = (
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -24,3 +24,12 @@ class Advertisement(models.Model):
             created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html('<span style="color: green;font-weight: bold;">Сегодня в {}</span>',created_time)
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description="Дата обновления")
+    def updated_at(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.update_at.date() == timezone.now().date():
+            updated_time = self.update_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: orange;font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.update_at.strftime("%d.%m.%Y в %H:%M:%S")
