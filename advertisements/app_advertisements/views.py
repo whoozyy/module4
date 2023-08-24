7d57c8683f06df10eeb3b4c93b2f77f22b6945ce
Sisyann <ezdvs16@gmail.com>
2023-08-24 20:35:35 +0500

    'initial'

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -6,10 +6,10 @@ from django.urls import reverse
 def index(request):
     advertisements= Advertisement.objects.all()
     context={"advertisements":advertisements}
-    return render(request,"index.html",context)
+    return render(request,"app_advertisements/index.html",context)
 
 def top_sellers(request):
-    return render(request,"top-sellers.html")
+    return render(request,"app_advertisements/top-sellers.html")
 
 def advertisement_post(request):
     if request.method == "POST":
@@ -24,15 +24,15 @@ def advertisement_post(request):
     else:
         form = AdvertisementForm()
     context = {"form":form}
-    return render(request,"advertisement-post.html",context)
+    return render(request,"app_advertisements/advertisement-post.html",context)
 
 def advertisement(request):
-    return render(request,"advertisement.html")
+    return render(request,"app_advertisements/advertisement.html")
 
 def register(request):
-    return render(request,"register.html")
+    return render(request,"app_auth/register.html")
 
 def login(request):
-    return render(request,"login.html")
+    return render(request,"app_auth/login.html")
 def profile(request):
-    return render(request,"profile.html")
+    return render(request,"app_auth/profile.html")
