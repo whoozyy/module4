4caeb23b94463297e2371de10e7f8a01f673206b
Sisyann <ezdvs16@gmail.com>
2023-08-24 20:33:45 +0500

    'initial'

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('lesson_4/',include('app_lesson_4.urls')),
     path('',include('app_advertisements.urls')),
+    path('myauth/',include('app_auth.urls'))
 ]
 
 if settings.DEBUG:
