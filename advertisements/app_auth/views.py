82dd2a74fc63844ce3ee57a5c1ccf43fc7904b43
Sisyann <ezdvs16@gmail.com>
2023-08-24 21:17:22 +0500

    'initial'

--- advertisements/app_auth/views.py
+++ advertisements/app_auth/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate,login
+def profile_view(request):
+    return render(request,'app_auth/profile.html')
+def login_view(request):
+    redirect_url = reverse('profile')
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect(redirect_url)
+        else:
+            return render(request,'app_auth/login.html')
 
-# Create your views here.
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request,username=username,password=password)
+    if user is not None:
+        login(request,user)
+        return redirect(redirect_url)
+    return render(request,'app_auth/login.html',{"error":"Пользователь не найден"})
